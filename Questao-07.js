41e4199cf778bddd135c1820d6441b3f165b6a7f
NikaelEnzo <enzo.marthins@gmail.com>
2024-04-10 14:55:56 -0400

    Questão 07 concluida

---
+++ Questao-07.js
@@ -0,0 +1,7 @@
+// Questão 7: Escreva um código JavaScript utilizando o operador ternário para determinar se um número é maior que 100. A variável numero contém o valor a ser verificado. Atribua o resultado à variável resultado.
+
+let numero = 100
+
+let resultado = (numero > 100) ? "É maior que 100" : (numero < 100) ? "É menor que 100" : "É igual"
+
+console.log(resultado)

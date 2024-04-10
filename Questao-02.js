93968743ae01a477d7826f7627eea359aea42dca
NikaelEnzo <enzo.marthins@gmail.com>
2024-04-10 14:51:45 -0400

    Questão 02 concluida

---
+++ Questao-02.js
@@ -0,0 +1,7 @@
+// Questão 2: Escreva um código JavaScript utilizando o operador ternário para determinar se um aluno foi aprovado ou reprovado em um exame. A variável nota contém a pontuação do aluno. Atribua o resultado à variável status.
+
+let nota = 6;
+
+let status = (nota >= 6) ? "Aprovado" : "Reprovado";
+
+console.log(status);

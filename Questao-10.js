8090fa7b7756c8aebb1ac1c14c2c15709d22805a
NikaelEnzo <enzo.marthins@gmail.com>
2024-04-10 15:20:03 -0400

    Questão 10 concluida

---
+++ Questao-10.js
@@ -0,0 +1,7 @@
+// Questão 10: Escreva um código JavaScript utilizando o operador ternário para determinar se uma pessoa pode entrar em um clube. A variável idade contém a idade da pessoa e a variável temConvite indica se ela possui um convite. Atribua o resultado à variável status. 
+
+let idade = 10
+let temConvite = true
+let status = (idade >= 14 && temConvite) ? "Pode entrar em um clube" : "Não pode entrar em um clube"
+
+console.log(status)
